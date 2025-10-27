4407d687b6341541fcc9cd9c96654a28b20cc939
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2025-10-26 20:40:33 -0500

    refactor(ai): update generateImageToolPrompt function signature and streamline message handling in image generation

--- src/app/actions/generate-image.tsx
+++ src/app/actions/generate-image.tsx
@@ -2,7 +2,7 @@
 
 import { generateImageToolPrompt } from "@/ai/prompts";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { generateText } from "ai";
+import { generateText, UserModelMessage } from "ai";
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GOOGLE_API_KEY
@@ -10,27 +10,24 @@ const google = createGoogleGenerativeAI({
 
 export async function generateImage(prompt: string) {
   try {
+
+    const message = {
+      role: 'user',
+      content: generateImageToolPrompt(prompt)
+    } as UserModelMessage;
+
     const result = await generateText({
       model: google('gemini-2.0-flash-preview-image-generation'),
       providerOptions: {
         google: { responseModalities: ['TEXT', 'IMAGE'] }
       },
-      messages: [
-        {
-          role: 'user',
-
-          parts: [{
-            type: 'text',
-            text: generateImageToolPrompt(prompt)
-          }]
-        }
-      ]
+      messages: [message]
     });
-    
+
     const image = result.files[0];
     return image;
   } catch (error) {
     console.error('Error generating image:', error);
-    return { status: 500,error: 'Failed to generate image' };
+    return { status: 500, error: 'Failed to generate image' };
   }
 }
